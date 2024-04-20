952ead9455b33b94ea48c7cd4e9929f1a9a5f39e
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-20 15:44:56 +0200

    fix: use serde default for optimizer

--- src/artifacts/mod.rs
+++ src/artifacts/mod.rs
@@ -270,6 +270,8 @@ pub struct Settings {
     pub stop_after: Option<String>,
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub remappings: Vec<Remapping>,
+    /// Custom Optimizer settings
+    #[serde(default)]
     pub optimizer: Optimizer,
     /// Model Checker options.
     #[serde(default, skip_serializing_if = "Option::is_none")]
