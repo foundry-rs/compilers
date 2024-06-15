3815a311e8a91af27f1da1f06b8f21dd2d08a45f
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-15 08:10:00 +0200

    chore: move lints to workspace

--- crates/project/src/lib.rs
+++ crates/project/src/lib.rs
@@ -1,7 +1,5 @@
 #![doc = include_str!("../../../README.md")]
-#![warn(rustdoc::all)]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 #[macro_use]
