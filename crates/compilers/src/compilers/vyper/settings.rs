651a86bab1d837d5f0965f3efd6201b26f624142
Arsenii Kulikov <klkvrr@gmail.com>
2024-08-15 03:48:57 +0800

    fix

--- crates/compilers/src/compilers/vyper/settings.rs
+++ crates/compilers/src/compilers/vyper/settings.rs
@@ -10,13 +10,20 @@ impl CompilerSettings for VyperSettings {
     }
 
     fn can_use_cached(&self, other: &Self) -> bool {
-        let Self { evm_version, optimize, bytecode_metadata, output_selection, search_paths } =
-            self;
+        let Self {
+            evm_version,
+            optimize,
+            bytecode_metadata,
+            output_selection,
+            search_paths,
+            experimental_codegen,
+        } = self;
         evm_version == &other.evm_version
             && optimize == &other.optimize
             && bytecode_metadata == &other.bytecode_metadata
             && output_selection.is_subset_of(&other.output_selection)
             && search_paths == &other.search_paths
+            && experimental_codegen == &other.experimental_codegen
     }
 
     fn with_include_paths(mut self, include_paths: &BTreeSet<PathBuf>) -> Self {
