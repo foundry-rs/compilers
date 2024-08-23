578e481925b7578226290248e61956cd797052cf
Jrigada <jrigada@est.frba.utn.edu.ar>
2024-08-23 11:33:32 -0300

    Update zksolc to 1.5.2

--- crates/compilers/src/compilers/zksolc/mod.rs
+++ crates/compilers/src/compilers/zksolc/mod.rs
@@ -38,7 +38,7 @@ pub use settings::ZkSolcSettings;
 
 pub const ZKSOLC: &str = "zksolc";
 pub const ZKSYNC_SOLC_RELEASE: Version = Version::new(1, 0, 1);
-pub const ZKSOLC_VERSION: Version = Version::new(1, 5, 1);
+pub const ZKSOLC_VERSION: Version = Version::new(1, 5, 2);
 
 #[derive(Debug, Clone, Serialize)]
 enum ZkSolcOS {
