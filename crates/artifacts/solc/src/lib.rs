e4a9b049d08e43899f9eebd2be56edaac04fef07
zerosnacks <95942363+zerosnacks@users.noreply.github.com>
2025-06-05 19:11:51 +0200

    debt: remove EOF version field (#279)
    EOF has been deprecated in Foundry
    
    Remove unused `serde_repr` dependency

--- crates/artifacts/solc/src/lib.rs
+++ crates/artifacts/solc/src/lib.rs
@@ -9,7 +9,6 @@ extern crate tracing;
 
 use semver::Version;
 use serde::{de::Visitor, Deserialize, Deserializer, Serialize, Serializer};
-use serde_repr::{Deserialize_repr, Serialize_repr};
 use std::{
     collections::{BTreeMap, HashSet},
     fmt,
@@ -272,16 +271,6 @@ pub struct Settings {
     /// If this key is an empty string, that refers to a global level.
     #[serde(default)]
     pub libraries: Libraries,
-    /// Specify EOF version to produce.
-    #[serde(default, skip_serializing_if = "Option::is_none")]
-    pub eof_version: Option<EofVersion>,
-}
-
-/// Available EOF versions.
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize_repr, Deserialize_repr)]
-#[repr(u8)]
-pub enum EofVersion {
-    V1 = 1,
 }
 
 impl Settings {
@@ -562,7 +551,6 @@ impl Default for Settings {
             libraries: Default::default(),
             remappings: Default::default(),
             model_checker: None,
-            eof_version: None,
         }
         .with_ast()
     }
