531e2986de641de76a7dc569120a6cfcdd92fac2
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-10-01 19:58:36 +0200

    chore: remove feature(doc_auto_cfg)

--- crates/artifacts/vyper/src/lib.rs
+++ crates/artifacts/vyper/src/lib.rs
@@ -1,7 +1,7 @@
 //! Vyper artifact types.
 
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod settings;
 pub use settings::{VyperOptimizationMode, VyperSettings};
