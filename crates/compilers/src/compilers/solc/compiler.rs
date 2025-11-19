foundry-rs/compilers

commit 8f1c61c8fe423e7b67d8b40c6bee60b8478f8f7e

fix: preserve pre release version (#340)
- preserve version, including prerelease if any, and pass to svm-rs
- needed in `find_svm_installed_version` to identify local binary and
avoid installing it on each run
- svm-rs logic to deal with looking up and installing prereleases in
https://github.com/alloy-rs/svm-rs/pull/176

crates/compilers/src/compilers/solc/compiler.rs
@@ -215,12 +215,11 @@ impl Solc {
     #[instrument(skip_all)]
     #[cfg(feature = "svm-solc")]
     pub fn find_svm_installed_version(version: &Version) -> Result<Option<Self>> {
-        let version = Version::new(version.major, version.minor, version.patch);
         let solc = svm::version_binary(&version.to_string());
         if !solc.is_file() {
             return Ok(None);
         }
-        Ok(Some(Self::new_with_version(&solc, version)))
+        Ok(Some(Self::new_with_version(&solc, version.clone())))
     }
 
     /// Returns the directory in which [svm](https://github.com/roynalnaruto/svm-rs) stores all versions
@@ -294,25 +293,18 @@ impl Solc {
         #[cfg(test)]
         crate::take_solc_installer_lock!(_lock);
 
-        let version = if version.pre.is_empty() {
-            Version::new(version.major, version.minor, version.patch)
-        } else {
-            // Preserve version if it is a prerelease.
-            version.clone()
-        };
-
         trace!("blocking installing solc version \"{}\"", version);
-        crate::report::solc_installation_start(&version);
+        crate::report::solc_installation_start(version);
         // The async version `svm::install` is used instead of `svm::blocking_install`
         // because the underlying `reqwest::blocking::Client` does not behave well
         // inside of a Tokio runtime. See: https://github.com/seanmonstar/reqwest/issues/1017
-        match RuntimeOrHandle::new().block_on(svm::install(&version)) {
+        match RuntimeOrHandle::new().block_on(svm::install(version)) {
             Ok(path) => {
-                crate::report::solc_installation_success(&version);
+                crate::report::solc_installation_success(version);
                 Ok(Self::new_with_version(path, version.clone()))
             }
             Err(err) => {
-                crate::report::solc_installation_error(&version, &err.to_string());
+                crate::report::solc_installation_error(version, &err.to_string());
                 Err(err)
             }
         }

commit 3409f90e67db5d4723dd7f555e74182987566e45

fix: handle solc versions with +commit (#341)
-
https://github.com/foundry-rs/compilers/commit/8f1c61c8fe423e7b67d8b40c6bee60b8478f8f7e
introduced an issue with versions like `0.8.15+commit.e14f2714` format
that can be seen in foundry CI failure
https://github.com/foundry-rs/foundry/actions/runs/19511930077/job/55854689276?pr=12599#step:14:9999
- these versions should be normalized to e.g. `0.8.15`
- only prereleases like `0.8.31-pre.1+commit.b59566f6` or `0.8.31-pre.1`
should be preserved and looked up with original version
- fix to preserve versions only if prereleases, foundry CI passes with
patch: https://github.com/foundry-rs/foundry/pull/12599
- should have created the foundry PR with patched versions to changes
first, sorry for back and forth :melting_face:

crates/compilers/src/compilers/solc/compiler.rs
@@ -215,11 +215,17 @@ impl Solc {
     #[instrument(skip_all)]
     #[cfg(feature = "svm-solc")]
     pub fn find_svm_installed_version(version: &Version) -> Result<Option<Self>> {
+        let version = if version.pre.is_empty() {
+            Version::new(version.major, version.minor, version.patch)
+        } else {
+            // Preserve version if it is a prerelease.
+            version.clone()
+        };
         let solc = svm::version_binary(&version.to_string());
         if !solc.is_file() {
             return Ok(None);
         }
-        Ok(Some(Self::new_with_version(&solc, version.clone())))
+        Ok(Some(Self::new_with_version(&solc, version)))
     }
 
     /// Returns the directory in which [svm](https://github.com/roynalnaruto/svm-rs) stores all versions
@@ -293,18 +299,25 @@ impl Solc {
         #[cfg(test)]
         crate::take_solc_installer_lock!(_lock);
 
+        let version = if version.pre.is_empty() {
+            Version::new(version.major, version.minor, version.patch)
+        } else {
+            // Preserve version if it is a prerelease.
+            version.clone()
+        };
+
         trace!("blocking installing solc version \"{}\"", version);
-        crate::report::solc_installation_start(version);
+        crate::report::solc_installation_start(&version);
         // The async version `svm::install` is used instead of `svm::blocking_install`
         // because the underlying `reqwest::blocking::Client` does not behave well
         // inside of a Tokio runtime. See: https://github.com/seanmonstar/reqwest/issues/1017
-        match RuntimeOrHandle::new().block_on(svm::install(version)) {
+        match RuntimeOrHandle::new().block_on(svm::install(&version)) {
             Ok(path) => {
-                crate::report::solc_installation_success(version);
+                crate::report::solc_installation_success(&version);
                 Ok(Self::new_with_version(path, version.clone()))
             }
             Err(err) => {
-                crate::report::solc_installation_error(version, &err.to_string());
+                crate::report::solc_installation_error(&version, &err.to_string());
                 Err(err)
             }
         }